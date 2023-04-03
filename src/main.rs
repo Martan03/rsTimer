f676373c10c0294d514dc754319d5046a1b30f82
Martan03 <martinslezak03@gmail.com>
2023-04-03 11:34:22 +0200

    Add printing time to the center

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use std::{time::{Instant}};
 
-use num_parser::print_number;
+use crate::num_parser::{print_time, get_time};
+
+
 mod num_parser;
 
 fn main() {
+    print!("\x1b[?1049h\x1b[J");
     let device_state = DeviceState::new();
-    let start = Instant::now();
 
     let mut con = true;
-    let digits = 1.0;
-    let mut last = -digits;
-
+    let decimals = 1.0;
+    let mut last = -decimals;
+    
+    let start = Instant::now();
     while con {
         let keys: Vec<Keycode> = device_state.get_keys();
         for key in keys.iter() {
             if key == &Keycode::Space {
-                println!("\x1b[H\x1b[J");
-                print_number(start.elapsed().as_secs_f64());
+                print_time(get_time(start.elapsed().as_secs_f64(), decimals as usize));
                 con = false;
             }
         }
         
-        let time = (start.elapsed().as_secs_f64() * digits * 10.0).round();
-        if time - digits <= last {
+        let time = (start.elapsed().as_secs_f64() * decimals * 10.0).round();
+        if time - decimals < last {
             continue;
         }
+
         last = time;
-        println!("\x1b[H\x1b[J");
-        print_number(start.elapsed().as_secs_f64());
+        print_time(get_time(start.elapsed().as_secs_f64(), decimals as usize));
     }
+    print!("\x1b[?1049l");
 }
--- src/num_parser.rs
+++ src/num_parser.rs
@@ -3,20 +3,35 @@ use self::digits::{get_digits};
 #[path = "digits.rs"]
 mod digits;
 
-pub fn print_number(num: f64) {
+pub fn get_time(num: f64, decimals: usize) -> Vec<String> {
     let digit_lines = get_digits();
     let mut numbers: Vec<String> = Vec::new();
     numbers.resize_with(5,  String::new);
 
-    let number = format!("{:.3}", num);
+    let number = format!("{:.1$}", num, decimals);
 
     for digit in number.chars() {
         for i in 0..5 as usize {
             numbers[i].push_str(digit_lines[&digit][i]);
         }
     }
+    numbers
+}
 
-    for line in numbers {
-        println!("{line}");
+fn get_time_length(time: &Vec<String>) -> usize {
+    if time.len() <= 0 {
+        return 0;
+    }
+    return time[0].len();
+}
+
+pub fn print_time(time: Vec<String>) {
+    let (w, h) = termion::terminal_size().unwrap();
+    let px = (w as usize - get_time_length(&time)) / 2;
+    let mut py = (h as usize - 5) / 2;
+
+    for line in time {
+        println!("\x1b[{py};{px}H{line}");
+        py += 1;
     }
 }
