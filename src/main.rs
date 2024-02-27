519e51c172b8030503e6944cfde4d47c347b01b6
Martan03 <martinslezak03@gmail.com>
2024-02-27 14:45:46 +0100

    Edit help to use termint

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::{
 };
 
 use eyre::Result;
+use termint::{
+    enums::fg::Fg,
+    help,
+    widgets::{grad::Grad, span::StrSpanExtension},
+};
 
 use crate::{
     gameloop::Game, stats::stats::Stats, stats_manager::StatsManager,
@@ -29,15 +34,15 @@ fn main() -> Result<()> {
 
     for arg in env::args().skip(1) {
         match arg.as_str() {
-            "-a" => {
+            "-a" | "--add" => {
                 add_session()?;
                 return Ok(());
             }
-            "-l" => {
+            "-l" | "--list" => {
                 list_sessions()?;
                 return Ok(());
             }
-            "-h" => {
+            "-h" | "--help" => {
                 help();
                 return Ok(());
             }
@@ -102,32 +107,25 @@ fn list_sessions() -> Result<()> {
 
 /// Displays help
 fn help() {
-    print!("Welcome to help for \x1b[92mrsTimer\x1b[0m by ");
-    // Prints name with color gradient
-    let name = "Martan03";
-    let r = 0;
-    let g = 220;
-    for i in 0..name.len() {
-        print!(
-            "\x1b[38;2;{};{};255m{}",
-            r + i * 25,
-            g - i * 20,
-            name.chars().nth(i).unwrap()
-        );
-    }
-    println!("\n\n\x1b[92mUsage: \x1b[97mrstimer \x1b[90m[flags]\n");
-    println!("\x1b[92mFlags:\n\x1b[0m");
-    println!("\x1b[93m  -h --help");
-    println!("\x1b[0m    Displays help\n");
-    println!("\x1b[93m  -l");
-    println!("\x1b[0m    Lists all sessions\n");
-    println!("\x1b[93m  -a");
-    println!("\x1b[0m    Opens dialog to add session\n");
+    println!(
+        "Welcome to help for {} by {}\n",
+        "rsTimer".fg(Fg::Green),
+        Grad::new("Martan03", (0, 220, 255), (175, 80, 255))
+    );
+    help!(
+        "Usage":
+        "\n-h --help" => "Displays this help"
+        "\n-l --list" => "Lists all sessions"
+        "\n-a --add" => "Opens dialog to add new session"
+    );
 }
 
 /// Prints invalid usage message
 /// * 'msg' - message
 fn invalid_usage(msg: &str) {
-    eprint!("\x1b[91mInvalid usage:\x1b[0m {msg}. ");
-    eprintln!("Type \x1b[93mtimer -h\x1b[0m to display help");
+    eprintln!(
+        "{} {msg}. Type {} to display help",
+        "Invalid usage:".fg(Fg::Red),
+        "rstimer -h".fg(Fg::Yellow)
+    );
 }
